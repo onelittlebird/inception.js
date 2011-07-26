d7380ee7b34fc2d28d578331ebb9a8091ad8cf49
Filip Moberg <fillzter@gmail.com>
2011-07-26 12:05:08 +0300

    Edited fquery.js via GitHub

--- fquery.js
+++ fquery.js
@@ -122,7 +122,9 @@ function fQuery() {
 				}
 
 				return c;
-			}
+			},
+
+                        jQextend : {hasOwn:Object.prototype.hasOwnProperty,isPlainObject:function(obj){if(!obj||typeof obj!=="object"||obj.nodeType||this.isWindow(obj)){return false}if(obj.constructor&&!this.hasOwn.call(obj,"constructor")&&!this.hasOwn.call(obj.constructor.prototype,"isPrototypeOf")){return false}var key;for(key in obj){}return key===undefined||this.hasOwn.call(obj,key)},isWindow:function(obj){return obj&&typeof obj==="object"&&"setInterval"in obj},isArray:Array.isArray||function(obj){return typeof obj==="array"},extend:function(){var options,name,src,copy,copyIsArray,clone,target=arguments[0]||{},i=1,length=arguments.length,deep=false;if(typeof target==="boolean"){deep=target;target=arguments[1]||{};i=2}if(typeof target!=="object"&&typeof target!=="function"){target={}}if(length===i){target=this;--i}for(;i<length;i++){if((options=arguments[i])!=null){for(name in options){src=target[name];copy=options[name];if(target===copy){continue}if(deep&&copy&&(this.isPlainObject(copy)||(copyIsArray=this.isArray(copy)))){if(copyIsArray){copyIsArray=false;clone=src&&this.isArray(src)?src:[]}else{clone=src&&this.isPlainObject(src)?src:{}}target[name]=this.extend(deep,clone,copy)}else if(copy!==undefined){target[name]=copy}}}}return target}}
 		};
 
 
