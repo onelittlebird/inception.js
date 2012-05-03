df689b7caa492543430ba0173b15edb7884c73e3
Filip Moberg <fillzter@gmail.com>
2012-05-03 15:28:32 +0300

    Update 1.2.2 - onReady functions are now also accessible from its parent object.

--- inception-1.2.1.js
+++ inception-1.2.1.js
@@ -265,48 +265,44 @@
 							}
 						}
 						break;
-
-					// Construct function as ordinary
-					default:
-						return function() {
-
-							if (typeof this[o.func] === "function") {
-
-								// Set up selector
-								this[o.func].selector = this[o.func].$ = core.selector;
-
-								// Set up selector (for new methods, if the current method is used as a constructor)
-								this[o.func].prototype.selector = this[o.func].prototype.$ = core.selector;
-
-								// Set up node
-								this[o.func].node = core.clone(core.node);
-
-								// This line is for legacy purposes only (where the selector is called from the global object)
-								window[core.instance].$ = window[core.instance].selector = core.selector;
-							}
-
-							// Execute user function in correct environment
-							if (typeof o.wrapper === "function") {
-								var self = this;
-								var args = arguments;
-
-								// Execute user function inside wrappers (if available)
-								return o.wrapper.call({
-									call: function() {
-
-										// Apply method with its parent object as reference
-										return o.method.apply(self, args);
-									}
-								});
-							} else {
-								// Execute user function as ordinary (if there are no wrappers defined for this object)
-								return o.method.apply(this, arguments);
-							}
-						};
-
-						break;
 				}
 
+				// Construct function as ordinary
+				return function() {
+
+					if (typeof this[o.func] === "function") {
+
+						// Set up selector
+						this[o.func].selector = this[o.func].$ = core.selector;
+
+						// Set up selector (for new methods, if the current method is used as a constructor)
+						this[o.func].prototype.selector = this[o.func].prototype.$ = core.selector;
+
+						// Set up node
+						this[o.func].node = core.clone(core.node);
+
+						// This line is for legacy purposes only (where the selector is called from the global object)
+						window[core.instance].$ = window[core.instance].selector = core.selector;
+					}
+
+					// Execute user function in correct environment
+					if (typeof o.wrapper === "function") {
+						var self = this;
+						var args = arguments;
+
+						// Execute user function inside wrappers (if available)
+						return o.wrapper.call({
+							call: function() {
+
+								// Apply method with its parent object as reference
+								return o.method.apply(self, args);
+							}
+						});
+					} else {
+						// Execute user function as ordinary (if there are no wrappers defined for this object)
+						return o.method.apply(this, arguments);
+					}
+				};
 			},
 
 			// Error handling
