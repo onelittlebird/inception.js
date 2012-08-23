16e48bb55cc2a7d3ae4be938456c90aebe2bafa1
Filip Moberg <filip@mcsquare.se>
2012-08-23 19:24:46 +0200

    Typo

--- kickstart.node.js
+++ kickstart.node.js
@@ -1,7 +1,11 @@
-module.exports = {
-	include: function(file_) {
-		with (global) {
-			eval(require('fs').readFileSync(file_).toString());
-		};
+
+	// A module for including inception.js (or any javascript for that matter) into the global namespace with node.js
+
+	module.exports = {
+		kickstart: function(file_) {
+			with (global) {
+				eval(require('fs').readFileSync(file_).toString());
+			};
+		}
 	}
-}
+
